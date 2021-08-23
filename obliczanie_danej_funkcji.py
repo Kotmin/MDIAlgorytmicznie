405a89a262e25a5f260ace394cc6be80ddf0fa39
Kotmin <70173732+Kotmin@users.noreply.github.com>
2021-08-23 23:08:25 +0200

    wiele argumentów

--- obliczanie_danej_funkcji.py
+++ obliczanie_danej_funkcji.py
@@ -1,10 +1,13 @@
 
-
+podejrzane = [1,-3,-2,2]
 
 def zwroc(x):
-    y =(3*x**4)+(4*x**3)-(12*x**2)+5 #tu wpisz funkcje
+    y =2*x**3 + 3*x**2 - 12*x+ 5 #tu wpisz funkcje
     print("Dla x= ",x,"zwraca :",y)
     return y
 
-zwroc(-2)
-zwroc(1)
+# zwroc(-2)
+# zwroc(1)
+
+for i in podejrzane:
+    zwroc(i)
